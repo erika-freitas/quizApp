a16c498edb588276b1c55c3a6c6a3af813310989
erikafreitas <erika.araujo@4yousee.com.br>
2021-12-28 16:30:44 -0300

    Fix link between pages

--- game.js
+++ game.js
@@ -108,7 +108,7 @@ startGame = () => {
 getNewQuestion = () => {
   if(availableQuestions.length === 0 || questionCounter >=MAX_QUESTIONS) {
     //go to the end page
-    return window.location.assign('/end.html');
+    return window.location.assign('end.html');
   }
   questionCounter++;
   questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
